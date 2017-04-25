04c43b2f97c842b5d7b85e2282a41d4fbeda3f76
oukka <annika@giosg.com>
2017-04-25 15:02:31 +0300

    The final version of the gesture controlled lights

--- new_version.js
+++ new_version.js
@@ -7,15 +7,21 @@ var board = new five.Board();
 
 var frame;
 var i = 0;
-
+var j = 0;
 
 var ledoff = true;
+
+// Define some variables for controlling colors
 var prev_color = "#000000";
+var fixedColor = false;
+var rainbow = ["#FF1744", "#FF4081", "#E040FB", "#651FFF", "#2196F3", "#40C4FF", "#00B8D4", "#00BFA5", "#00E676", "#EEFF41", "#FFEB3B", "#FFC400", "#FF9100", "#FF3D00", "#FF3D00"];
+var goRainbow = false;
+var index = 0;
 var color = '';
 var bright_index = 99;
 
-
-var hand_x, hand_z;
+var hand_x, hand_y, hand_z;
+var grab = false;
 
 
 //Function to convert hex format to a rgb color
@@ -42,6 +48,19 @@ function getrgb(x, y, z) {
 
 }
 
+function changeColorsInRainbow(led) {
+    if (index >= rainbow.length) {
+      index = 0;
+    }
+    led.color(rainbow[index]);
+    j++;
+    if(j>=40) {
+        index++;
+        j=0;
+    }
+}
+
+// Controlling brightness
 function changeBrightness(y) {
     return (y/600)*100;
 }
@@ -55,11 +74,10 @@ board.on('ready', function() {
         // track only 40frame/s
         if (i % 3 === 0) {
             frame = new LeapFrame(data);
-            if (frame.valid) {
-                // console.log(frame.data.hands);
-                // basis.move(frame.palmPosition.x);
-            }
             i = 0;
+        }
+        if (goRainbow === true) {
+            changeColorsInRainbow(led);
         }
     });
 
@@ -70,27 +88,56 @@ board.on('ready', function() {
         hand_y = hand.palmPosition[1];
         hand_z = hand.palmPosition[2];
 
-        if (hand.grabStrength > 0.7) {
-            led.off();
-            // ledoff = true;
-        } else {
-            led.on();
-            // ledoff = false;
-            // console.log(hand.palmPosition);
-            // console.log(hand.indexFinger.extended);
-            if (hand.indexFinger.extended && !hand.thumb.extended && !hand.middleFinger.extended && !hand.ringFinger.extended && !hand.pinky.extended) {
-                bright_index = changeBrightness(hand_y);
-                led.intensity(bright_index);
-            } else {
-                color = getrgb(hand_x, hand_y, hand_z);
-                prev_color = color;
-                led.color(color);
-            }
+        var timeHandVisible = hand.timeVisible;
 
+        if (ledoff && timeHandVisible > 1.3 && hand_y >= 50) {
+            led.on();
+            ledoff = false;
         }
 
+        if (!ledoff) {
+            if (hand.type === 'right') {
+                goRainbow = false;
+                if (hand.indexFinger.extended && !hand.thumb.extended && !hand.middleFinger.extended && !hand.ringFinger.extended && !hand.pinky.extended) {
+                    bright_index = changeBrightness(hand_y);
+                    led.intensity(bright_index);
+                } else if (hand.indexFinger.extended && hand.thumb.extended && hand.middleFinger.extended && hand.ringFinger.extended && hand.pinky.extended) {
+                    if (!fixedColor) {
+                        color = getrgb(hand_x, hand_y, hand_z);
+                        prev_color = color;
+                        led.color(color);
+                    }
+                    if (hand.grabStrength > 0.8) {
+                        fixedColor = true;
+                    } else {
+                        fixedColor = false;
+                    }
+                }
+                // else if (hand.indexFinger.extended && !hand.thumb.extended && hand.middleFinger.extended && !hand.ringFinger.extended && !hand.pinky.extended) {
+                //     console.log(index);
+                //     if (index >= rainbow.length) {
+                //       index = 0;
+                //     }
+                //     led.color(rainbow[index]);
+                //     //index++;
+                //     j++;
+                //     if(j>=10) {
+                //         index++;
+                //         j=0;
+                //     }
+                // }
+                // else if (hand.indexFinger.extended && hand.thumb.extended && hand.middleFinger.extended && !hand.ringFinger.extended && !hand.pinky.extended) {
+                //     led.blink(500);
+                // }
+                if (hand_y < 50) {
+                    led.off();
+                    ledoff = true;
+                }
+            } else if (hand.type == 'left') {
+                goRainbow = true;
+            }
+        }
     });
-
 
     controller.connect();
 });
